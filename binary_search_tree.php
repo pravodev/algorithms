d3fd1a1ab272ac1ff41dd89aa4ef39ea91e3a153
Rifqi Khoeruman Azam <pravodev@gmail.com>
2019-11-09 19:50:18 +0700

    add isBinarySearchTree method

--- binary_search_tree.php
+++ binary_search_tree.php
@@ -1,14 +1,22 @@
 <?php
+/**
+ * Binary Search Tree
+ * 
+ * @author Rifqi Khoeruman Azam <pravodev@gmail.com>
+ */
 
 class Node
 {
-    private $left, $right, $data = null;
+    public $left, $right, $data = null;
 
     public function __construct($data)
     {
         $this->data = $data;
     }
 
+    /**
+     * Get position for $value given 
+     */
     public function getPositionOf($value)
     {
         if($value < $this->data){
@@ -68,8 +76,37 @@ class Node
         }
         
     }
+
+    public static function isBinarySearchTree($node, $min = PHP_INT_MIN, $max = PHP_INT_MAX): bool
+    {
+        if($node !== null && !is_a($node, Node::class)){
+            throw new \Exception('$node must be an instance of Node class');
+        }
+        
+        if($node === null){
+            return true;
+        }
+
+        if($node->data < $min || $node->data > $max){
+            return false;
+        }
+
+        return self::isBinarySearchTree($node->left, $min, $node->data-1) && self::isBinarySearchTree($node->right, $node->data+1, $max);
+    }
 }
 
 $root = new Node(30);
 $root->insertMany([5,12,6,3,4,19,100,24,50,2,102]);
-echo ($root->find(100));
+print_r($root->find(100));
+print_r("\n\n\n");
+
+$second_root = new Node(20);
+$second_root->left = new Node(11);
+$second_root->right = new Node(21);
+$second_root->left->left = new Node(22);
+
+if(Node::isBinarySearchTree($second_root)){
+    print_r('** $second_root is binary search tree **');
+}else{
+    print_r('!! $second_root is not binary search tree !!');
+}
